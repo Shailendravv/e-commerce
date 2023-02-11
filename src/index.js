6b0f648acc50ca76a6c493ca413e56e6af652dd2
Shailendra <shailendra.c@shyenatechyarn>
2023-02-11 15:03:11 +0530

    e[-com project completed

--- src/index.js
+++ src/index.js
@@ -11,12 +11,26 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Domain
+// dev-5sz3fcrkyi86ovb2.us.auth0.com
+// ClientId
+// dFH5wHglEFjrUvalghESXQBR6VqyFoQ2
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
